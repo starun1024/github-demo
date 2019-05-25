22829b4dbd885b15767e13855b1f7fefc683afea
tarun <s.tarun1024@gmail.com>
2019-05-25 21:11:27 +0530

    New file added

--- Hello_World.java
+++ Hello_World.java
@@ -3,9 +3,12 @@ Class Hello_World
 
 	public static void main(String args[])
 	{
-
-	 System.out.println("Hello World");
+		print()
+	}
 	
+	private static void print()
+	{
+		System.out.println("Hello World");
 	}
 
 }
